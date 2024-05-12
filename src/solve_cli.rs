c14f26756462edd16d10e2957c73aab87f4743de
koelzk <kk@crispy-cow.de>
2024-05-12 16:18:49 +0200

    Add first code for solver app with Gtk-based UI

--- src/solve_cli.rs
+++ src/solve_cli.rs
@@ -66,18 +66,4 @@ fn main() {
             }
         }
     }
-    // for seed in 0..10_000 {
-    //     let mut rng = &mut StdRng::seed_from_u64(seed);
-    //     let board = Board::random(&mut rng);
-
-    //     let solver = Solver::new(&board);
-    //     let result = solver.solve(100_000, 100, true);
-
-    //     let result_str = match result.status {
-    //         SolveResultStatus::Solved => format!("{:3}", result.moves.len()),
-    //         SolveResultStatus::ReachedMaxIterations => "?".to_owned(),
-    //         SolveResultStatus::NoSolution => "-".to_owned(),
-    //     };
-    //     println!("{:4} => {}", seed, result_str);
-    // }
 }
